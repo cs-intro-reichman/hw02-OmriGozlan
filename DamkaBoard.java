f62e64c31be181545643c16aa4d98e2929a40653
omri.gozlan <omri.gozlan@post.runi.ac.il>
2023-12-29 19:21:56 +0200

    update for DamkaBoard

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,17 +4,15 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 		int N = Integer.parseInt(args[0]);
-		for(int i=0 ; i<N ; i++){
-			for(int j=0; j<N; j++){
-				if((i%2) == 0){
-					System.out.print("*");
-				}else if(j==0) {
-					System.out.print(" *");
+		for (int i = 0; i < N; i++) {
+			System.out.println();
+			for (int j = 0; j < N; j++) {
+				if ((i % 2) == 0) {
+					System.out.print("* ");
 				} else {
-					System.out.print("*");
+					System.out.println(" *");
 				}
 			}
-			System.out.println();
 		}
 	}
 }
